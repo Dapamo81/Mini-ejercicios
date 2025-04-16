919a95cb155234ee5c97ad0663d8d322961915f0
Dani <dpajaresfo@gmail.com>
2025-04-16 20:33:21 +0200

    añadimos mostrar datos

--- js/funcion.js
+++ js/funcion.js
@@ -8,3 +8,14 @@ function restarNumeros(){
     console.log("Resta:", num1," + ", num2," =", resultado);
 
 }
+
+//funcion para mostrar datos
+
+function mostrarDatos(){
+    const nombre = prompt("Ingresa tu nombre");
+    const localidad = prompt("¿En que localidad vives?");
+    const hobby = prompt("¿Cual es tu hobby?");
+    const frase = prompt("Te llamas "+ nombre+ ", vives en "+ localidad+ " y te gusta "+ hobby+ ".");
+    document.getElementById("resultado").textContent = frase;
+    console.log(frase);
+}
